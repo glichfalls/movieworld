4998f69b115d374b11ab71ace128f6efea89e8e3
Robin Portner <robin.portner@hotmail.com>
2020-11-11 21:10:27 +0100

    first slider version

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from "@angular/core";
+import {Component, DoCheck, Input, OnChanges, OnInit} from "@angular/core";
 import {Media} from "../../models/media.model";
 
 @Component({
@@ -6,11 +6,30 @@ import {Media} from "../../models/media.model";
     templateUrl: "./slider.component.html",
     styleUrls: ["./slider.component.scss"]
 })
-export class SliderComponent implements OnInit {
+export class SliderComponent implements OnChanges {
 
     @Input() movies: Array<Media>;
 
-    ngOnInit(): void {
+    public groups: Array<Array<Media>> = [];
+    private readonly items: number;
+
+    public constructor() {
+        this.items = Math.floor(window.innerWidth / 300);
+    }
+
+
+    ngOnChanges(changes: any): void {
+        const groups = Math.ceil(this.movies.length / this.items);
+        const movies = changes.movies.currentValue;
+        for (let i = 0; i < groups; i++) {
+            this.groups[i] = [];
+            for (let j = 0; j < this.items; j++) {
+                if(movies[(i * this.items) + j]) {
+                    this.groups[i][j] = movies[(i * this.items) + j];
+                }
+            }
+        }
+        console.log(this.groups);
     }
 
 }
