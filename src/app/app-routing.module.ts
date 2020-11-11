904495d8d9834e893378c79484ca382281862355
marinobantli <marino@maban.ch>
2020-11-11 19:49:15 +0100

    Added component "In theater" and the associated route Removed the click-event from the buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from "@angular/router";
 import {FrontPageComponent} from "./components/front-page/front-page.component";
 import {HottestMoviesComponent} from "./components/hottest-movies/hottest-movies.component";
 import {FavoriteMoviesComponent} from "./components/favorite-movies/favorite-movies.component";
+import {InTheaterMoviesComponent} from "./components/in-theater-movies/in-theater-movies.component";
 
 const routes: Routes = [
     {path: "", component: FrontPageComponent},
     {path: "hottest", component: HottestMoviesComponent},
     {path: "favorites", component: FavoriteMoviesComponent},
+    {path: "intheater", component: InTheaterMoviesComponent},
 ];
 
 @NgModule({
--- src/app/components/hottest-movies/hottest-movies.component.ts
+++ src/app/components/hottest-movies/hottest-movies.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Media, MovieResponse} from "../../models/media.model";
+import {MoviesService} from "../../services/movies.service";
 
 @Component({
   selector: 'app-hottest-movies',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hottest-movies.component.scss']
 })
 export class HottestMoviesComponent implements OnInit {
+      public movies: Array<Media> = [];
+      constructor(private moviesService: MoviesService) {
+          this.moviesService.getBestRatedMovies("de-CH", 1, "CH").subscribe((data: MovieResponse) => {
+              console.log(data);
+              this.movies = data.results;
+          });
+      }
 
-  constructor() { }
+    ngOnInit(): void {
 
-  ngOnInit(): void {
-  }
-
+    }
 }
